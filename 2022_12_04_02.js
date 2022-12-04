b742c4df101ce6b2ca0a87b4732aa344c53a92b4
nopor <9073633+nopor@users.noreply.github.com>
2022-12-04 10:43:38 +0000

    day 4

---
+++ 2022_12_04_02.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+const input = fs.readFileSync('2022_12_04_01.txt', 'utf8').toString();
+let result = 0;
+
+input.split('\n').every(v => {
+    const f = v.substring(0, v.indexOf(','));
+    const s = v.substring(v.indexOf(',') + 1);
+    const f1 = Number(f.substring(0,f.indexOf('-')));
+    const f2 = Number(f.substring(f.indexOf('-') + 1));
+    const s1 = Number(s.substring(0,s.indexOf('-')));
+    const s2 = Number(s.substring(s.indexOf('-') + 1));
+
+    if ((f1 >= s1 && f2 <= s2) ||
+        (s1 >= f1 && s2 <= f2) ||
+        (f1 >= s1 && f1 <= s2) ||
+        (s1 >= f1 && s1 <= f2) ||
+        (f2 >= s1 && f2 <= s2) ||
+        (s2 >= f1 && s2 <= f2)) {
+        result+=1;
+    }
+    return true;
+});
+
+console.log(result);
