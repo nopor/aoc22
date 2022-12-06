808a5cec94cefaef45de53a5b9fa9317659d5c0f
nopor <9073633+nopor@users.noreply.github.com>
2022-12-06 09:06:01 +0000

    cleanish day 6

---
+++ 2022_12_06_01_02.js
@@ -0,0 +1,32 @@
+const fs = require('fs');
+const input = fs.readFileSync('2022_12_06_01.txt', 'utf8').toString();
+
+let result = 0;
+const iarr = [...input];
+function timesPartOfStr(arr, char) {
+  let t = 0;
+  for (var i = 0; i < arr.length; i++)
+    if (arr[i] === char) t += 1;
+  return t;
+}
+function containsCharactersTwice(arr) {
+  let twice = false;
+  for(let i = 0; i < arr.length - 1; i++) {
+    if (timesPartOfStr(arr, arr[i]) > 1) {
+      twice = true;
+      break;
+    }
+  }
+  return twice;
+}
+
+iarr.every((v, i) => {
+  const message = [...input.substring(i, i + 14)];
+  if (!containsCharactersTwice(message)) {
+    result = i + 14;
+    return false;
+  }
+  return true;
+});
+
+console.log(result);
